3f25a42dd514a2603f4599c558e67c6e97e8d59f
dianarmiller <diana.r.miller@gmail.com>
2015-10-15 21:56:05 -0700

    add prompt box to user and list of multiple of threes

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,9 @@
 $(document).ready(function() {
-
+  $('html').click(function() {
+    alert ("Please enter a number!")
+  });
+  // 
+  // $('ul').text("<li>hello</li>");
+  // });
+  event.preventDefault();
 });
