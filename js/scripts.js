ab6d80682c0c0d86cc35c28a9e3ca602efc64dc1
dianarmiller <diana.r.miller@gmail.com>
2015-10-17 00:05:32 -0700

    add prompt box to user to input number, hide li in css, started code in js for ping pong but incomplete

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,11 @@
 $(document).ready(function() {
-  $('html').click(function() {
-    alert ("Please enter a number!")
-  });
-  // 
-  // $('ul').text("<li>hello</li>");
-  // });
+
+  inputNumber = prompt("Please enter a number to start playing", " ");
+  var inputNumber = ["myNumbers"];
+
+  myNumbers = ["3","9","15","18","25","27","30"]
+
+  $("inputNumbers").append("<li>" + inputNumbers + "</li>");
+
   event.preventDefault();
 });
