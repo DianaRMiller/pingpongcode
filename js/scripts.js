4e628ff02c3eea25d39172134c595fca2c04c527
dianarmiller <diana.r.miller@gmail.com>
2015-10-20 19:56:01 -0700

    add comments to keep working on code

--- js/scripts.js
+++ js/scripts.js
@@ -5,7 +5,16 @@ $(document).ready(function() {
 
   var index = 0
   for (index; index <= inputNumber; index += 1) {
-    $(".inputNumber").append("<li>" + index + "</li>");
+    // Now instead of printing index on each line
+    // I want see if index is divisible by 3
+    // if index is divisible by 3
+    // print the word ping
+    // var remainder = % (add a few things here)
+    // in the li's print index, and also print remainder
+
+    $(".inputNumber").append("<li> The number:" + index + "what is the remainder when you divide by 3?" + remainder +"</li>");
+    // The number: 0 what is the remainder when you divide by 3? 0
+    // The number: 1 what is the remainder when you divide by 3? 1
   }
 
   // var exampleNumbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15];
