9e821a06aa3ffdb6c978d7e409f6877d5496b50b
dianarmiller <diana.r.miller@gmail.com>
2015-10-26 22:33:10 -0700

    add changes to files and update code for scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,28 +1,22 @@
 $(document).ready(function() {
 
-  var inputNumber = prompt("What number would you like me to ping-pong", " ");
-  $(".inputNumber").text(inputNumber);
+  var inputNumber = prompt("What number would you like me to ping-pong");
+    inputNumber = parseInt(inputNumber);
 
-  var index = 0
-  for (index; index <= inputNumber; index += 1) {
-    // Now instead of printing index on each line
-    // I want see if index is divisible by 3
-    // if index is divisible by 3
-    // print the word ping
-    // var remainder = % (add a few things here)
-    // in the li's print index, and also print remainder
+  for (var index = 1; index <= inputNumber; index += 1) {
 
-    $(".inputNumber").append("<li> The number:" + index + "what is the remainder when you divide by 3?" + remainder +"</li>");
-    // The number: 0 what is the remainder when you divide by 3? 0
-    // The number: 1 what is the remainder when you divide by 3? 1
-  }
+      if ((index % 3 === 0) && (index % 5 === 0)) {
+        $(".inputNumber").append("<li>" + "ping-pong" + "</li>");
+      } if (index % 3 === 0) {
+        $(".inputNumber").append("<li>" + "ping" + "</li>");
+      } if (index % 5 === 0) {
+        $(".inputNumber").append("<li>" + "pong" + "</li>");
+      } else {
 
-  // var exampleNumbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15];
-  //
-  // exampleNumbers.forEach(function(exampleNumber) {
-  //   $('.inputNumber').append("<li>" + exampleNumber + "</li>");
-  // });
-
+      $(".inputNumber").append("<li>" + index + "</li>");
+    }
+}
 
   event.preventDefault();
+
 });
